1558f1171a922592252d8239f667b21233fb8bc5
Gabriel Bento <gab.silva.bento@gmail.com>
2023-09-14 14:32:56 -0300

    Criada classe de exceção

---
+++ src/exception/ParametrosInvalidosException.java
@@ -0,0 +1,11 @@
+package exception;
+
+public class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException(String message) {
+        super(message);
+    }
+
+    public ParametrosInvalidosException(){
+
+    }
+}
